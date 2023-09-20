d2669a65feba25a9c5406a76c143280ba57328ce
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-09-20 13:42:01 +0700

    tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from main.views import show_main
-from main.views import show_main, create_product, show_xml 
+from main.views import show_main, create_product, show_xml, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -8,4 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'), 
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,26 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
 
     context = {
-        'name': 'Pak Bepe', # Nama kamu
+        'name': request.user.username,
         'class': 'PBP A', # Kelas PBP kamu
-        'products': products
-    }
+        'products': products,
+        'last_login': request.COOKIES['last_login'],
+    }   
 
     return render(request, "main.html", context)
+
 def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
@@ -41,3 +51,36 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
